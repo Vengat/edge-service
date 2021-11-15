fb2cf970ae11f9876c2e3917c7a1a8fbc997ee13
Vengat <vengatramanan.ramar@jungleegames.com>
2021-11-15 14:37:35 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # edge-service
-This is an edge service prototype 
+<h3>This is an edge service prototype</h3>
+
+<h4>A route is defined by the following:</h4>
+
+<p>Predicates, which select a route based on information in the incoming HTTP request</p>
+
+<p>Filters, which can modify both the request and/or the response</p>
+<p>A destination URI, which describes where to send a request</p>
+<p>An ID, that is, the name of the route</p>
+<p>For a full list of available predicates and filters, refer to the reference documentation: https://cloud.spring.io/spring-cloud-gateway/single/spring-cloud-gateway.html.</p>
+ 
---
+++ src/main/java/com/vengat/tuts/edgeservice/config/HealthConfig.java
@@ -0,0 +1,54 @@
+package com.vengat.tuts.edgeservice.config;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.CompositeReactiveHealthContributor;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.ReactiveHealthContributor;
+import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class HealthConfig {
+	
+
+	@Autowired
+	WebClient webClient;
+	
+	@Bean
+	ReactiveHealthContributor healthCheckMicroServices() {
+		final Map<String, ReactiveHealthIndicator> registry = new LinkedHashMap<>();
+
+		registry.put("product", () -> getHealth("http://product"));
+		registry.put("recommendation",    () -> 
+		getHealth("http://recommendation"));
+		registry.put("review",            () -> 
+		getHealth("http://review"));
+		registry.put("product-composite", () -> 
+		getHealth("http://product-composite"));
+
+		return CompositeReactiveHealthContributor.fromMap(registry);
+
+	}
+
+	private Mono<Health> getHealth(String baseUrl) {
+
+		String url = baseUrl + "/actuator/health";
+		//		TODO: Enable logging
+				LOG.debug("Setting up a call to the Health API on URL: {}", 
+					      url);
+
+		return webClient.get().uri(url).retrieve()
+				.bodyToMono(String.class)
+				.map(s -> new Health.Builder().up().build())
+				.onErrorResume(ex -> 
+				Mono.just(new Health.Builder().down(ex).build()))
+				.log(LOG.getName(), FINE);
+	}
+}
